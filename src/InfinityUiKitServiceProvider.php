bdcc85bb60620cb6e28e323366977c8a497d8c25
tooinfinity <touwfiqdev@gmail.com>
2024-08-18 18:55:23 +0100

    folder structure and command

--- src/InfinityUiKitServiceProvider.php
+++ src/InfinityUiKitServiceProvider.php
@@ -1,60 +1,119 @@
 <?php
 
-namespace Tooinfinity\InfinityUiKit;
+declare(strict_types=1);
 
+namespace InfinityUiKit;
+
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\BladeCompiler;
+use InfinityUiKit\Console\PublishCommand;
+use Livewire\Livewire;
+use Illuminate\Support\Str;
+use Tooinfinity\InfinityUiKit\InfinityUiKit;
 
 class InfinityUiKitServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot()
+    public function register(): void
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'infinity-ui-kit');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'infinity-ui-kit');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->mergeConfigFrom(__DIR__ . '/../config/infinity-ui-kit.php', 'infinity-ui-kit');
 
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('infinity-ui-kit.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/infinity-ui-kit'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/infinity-ui-kit'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/infinity-ui-kit'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
+            $this->commands([
+                PublishCommand::class,
+            ]);
         }
     }
 
-    /**
-     * Register the application services.
-     */
-    public function register()
+    public function boot(): void
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'infinity-ui-kit');
+        $this->bootResources();
+        $this->bootBladeComponents();
+        $this->bootLivewireComponents();
+        $this->bootDirectives();
+        $this->bootPublishing();
+    }
 
-        // Register the main class to use with the facade
-        $this->app->singleton('infinity-ui-kit', function () {
-            return new InfinityUiKit;
+    private function bootResources(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'infinity-ui-kit');
+    }
+
+    private function bootBladeComponents(): void
+    {
+        $this->callAfterResolving(BladeCompiler::class, function (BladeCompiler $blade) {
+            $prefix = config('infinity-ui-kit.prefix', '');
+            $assets = config('infinity-ui-kit.assets', []);
+
+            /** @var BladeComponent $component */
+            foreach (config('infinity-ui-kit.components', []) as $alias => $component) {
+                $blade->component($component, $alias, $prefix);
+
+                $this->registerAssets($component, $assets);
+            }
         });
+    }
+
+    private function bootLivewireComponents(): void
+    {
+        // Skip if Livewire isn't installed.
+        if (!class_exists(Livewire::class)) {
+            return;
+        }
+
+        $prefix = config('infinity-ui-kit.prefix', '');
+        $assets = config('infinity-ui-kit.assets', []);
+
+        /** @var LivewireComponent $component */
+        foreach (config('infinity-ui-kit.livewire', []) as $alias => $component) {
+            $alias = $prefix ? "$prefix-$alias" : $alias;
+
+            Livewire::component($alias, $component);
+
+            $this->registerAssets($component, $assets);
+        }
+    }
+
+    private function registerAssets($component, array $assets): void
+    {
+        foreach ($component::assets() as $asset) {
+            $files = (array)($assets[$asset] ?? []);
+
+            collect($files)->filter(function (string $file) {
+                return Str::endsWith($file, '.css');
+            })->each(function (string $style) {
+                InfinityUiKit::addStyle($style);
+            });
+
+            collect($files)->filter(function (string $file) {
+                return Str::endsWith($file, '.js');
+            })->each(function (string $script) {
+                InfinityUiKit::addScript($script);
+            });
+        }
+    }
+
+    private function bootDirectives(): void
+    {
+        Blade::directive('bukStyles', function (string $expression) {
+            return "<?php echo InifinityUiKit\\InifinityUiKit::outputStyles($expression); ?>";
+        });
+
+        Blade::directive('bukScripts', function (string $expression) {
+            return "<?php echo InifinityUiKit\\InifinityUiKit::outputScripts($expression); ?>";
+        });
+    }
+
+    private function bootPublishing(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/infinity-ui-kit.php' => $this->app->configPath('infinity-ui-kit.php'),
+            ], 'blade-ui-kit-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => $this->app->resourcePath('views/vendor/infinity-ui-kit'),
+            ], 'infinity-ui-kit-views');
+        }
     }
 }
